86ecd4f485cb247bb44e6cb3e6336424f0d18170
oliviaf <olivia.flynn2@hotmail.com>
2021-02-19 13:58:05 +0000

    user movement working

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,6 @@ function init() {
 
 
   //button starts the game
-  
   function startGame() {
     console.log('game has comenced', startGame)
   }
@@ -18,9 +17,8 @@ function init() {
   const cells = []
 
   const lipstickClass = 'lipstick'
-
   const lipstickStartPosition = 95
-  let lipstickCurrentPosition = 0
+  let lipstickCurrentPosition = 95
 
 
   // creating the  game grid
@@ -41,17 +39,48 @@ function init() {
     cells[position].classList.add(lipstickClass)
   }
 
+  //* remove lipstick from grid 
+  function removeLipstick(position) {
+    cells[position].classList.remove(lipstickClass)
+  }
+
+  function handleKeyUp(event) {
+    const key = event.keyCode //event.keyCode finds out which key is clicked - each key registers a new number 
+
+    removeLipstick(lipstickCurrentPosition)
+    
+    if (key === 39 && lipstickCurrentPosition % width !== width - 1) { //right. stops the player being able to leave the grid
+      lipstickCurrentPosition++
+    } else if (key === 37 && lipstickCurrentPosition % width !== 0) { //left
+      lipstickCurrentPosition--
+    } else if (key === 38 && lipstickCurrentPosition >= width) { //up
+      lipstickCurrentPosition -= width
+    } else if (key === 40 && lipstickCurrentPosition + width <= width * width - 1) { //down
+      lipstickCurrentPosition += width
+    } else {
+      console.log('INVALID KEY')
+    }
+    
+    addLipstick(lipstickCurrentPosition)
+  }
+
+  // adding the goal - homes 
+  const lipsHomeClass = document.querySelector('.lipsHome')
+  const lipsPosition = 2
+
+  function addLips(position){
+    cells[position].classList.add('.lipsHome')
+    addLips(lipsPosition)
+  }
+
+  
 
 
 
 
 
 
-
-
-
-
-
+  document.addEventListener('keyup', handleKeyUp)
 
   startButton.addEventListener('click', startGame)
   createGrid(lipstickStartPosition)
